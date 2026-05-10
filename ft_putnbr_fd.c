fc6238a20f88838691d3f2e396316513eff5ab3b
Diana <dianabali64@gmail.com>
2026-05-10 16:49:15 +0200

    ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -13,15 +13,15 @@ void	ft_putnbr_fd(int n, int fd)
 {
     long int number;
 
-    number = n;
+    number = n; // To handle the case when n is INT_MIN we need to use a long int
     if (number < 0)
     {
-        ft_putchar_fd('-', fd);
-        number = number * -1;
+        ft_putchar_fd('-', fd); // Output the negative sign if the number is negative
+        number = number * -1; // Make the number positive for further processing
     }
     if (number >= 10)
-        ft_putnbr_fd(number / 10, fd);
-    ft_putchar_fd(number % 10 + '0', fd);
+        ft_putnbr_fd(number / 10, fd); // Recursively call the function with the number divided by 10 to process the next digit
+    ft_putchar_fd(number % 10 + '0', fd); // Output the last digit of the number by taking the modulus with 10 and converting it to a character
 }
 
 /* int main(void)
