f2981a4203304a07f262f5f603ceb2982b671931
Diana Bali <dianabali64@gmail.com>
2026-05-12 14:46:13 +0200

    ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -15,12 +15,12 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
-		if (*s == (unsigned char)c)
+		if (*(unsigned char *)s == (unsigned char)c)
 			return ((char *)s);
 		s++;
 	}
-  if ((unsigned char)c == '\0')
-    return ((char *)s);
+	if (*(unsigned char *)s == (unsigned char)c)
+		return ((char *)s);
 	return (NULL);
 }
 
